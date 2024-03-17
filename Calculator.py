908726bc99912f9f37cfc3cbda05f6128a0176a6
Lytvyn <163163709+VadimLytvyn@users.noreply.github.com>
2024-03-17 12:37:54 +0200

    метод, що знаходить найбільший спільний дільник для двох чисел на вході

--- Calculator.py
+++ Calculator.py
@@ -8,7 +8,10 @@ class TestCalculator():
     def test_calculator_power(self):
         calculator=Calculator()
         self.assertEqual(calculator.power(6,3),216)
-
+    def gcd(self,a,b):
+        while b:
+            a,b=b,a%b
+        return a
 if __name__=="__main__":
     unittest.main()
     
