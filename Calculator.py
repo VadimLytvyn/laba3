cca2db9da052acd86297452d7db8526071eae4f0
Lytvyn <163163709+VadimLytvyn@users.noreply.github.com>
2024-03-17 12:39:22 +0200

    метод, що знаходить найменше спільне кратне

--- Calculator.py
+++ Calculator.py
@@ -4,14 +4,30 @@ class Calculator():
     def power(self,a,b):
         return a**b
     
-class TestCalculator():
-    def test_calculator_power(self):
-        calculator=Calculator()
-        self.assertEqual(calculator.power(6,3),216)
     def gcd(self,a,b):
         while b:
             a,b=b,a%b
         return a
+
+    def lcm(self,a,b):
+        return a*b//self.gcd(a,b)
+     
+class TestCalculator():
+    def test_calculator_power(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.power(6,3),216)
+    def test_gcd(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.gcd(12,18),6)
+        self.assertEqual(calculator.gcd(15,25),5)
+        self.assertEqual(calculator.gcd(21,18),7)
+        
+    def test_lcm(self):
+        calculator=Calculator()
+        self.assertEqual(calculator.gcd(12,18),36)
+        self.assertEqual(calculator.gcd(15,25),75)
+        self.assertEqual(calculator.gcd(21,28),84)
+    
 if __name__=="__main__":
     unittest.main()
     
