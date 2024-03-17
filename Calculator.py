d78daa3f4f7042fe6fac7a62a5154d2402938803
Lytvyn <163163709+VadimLytvyn@users.noreply.github.com>
2024-03-17 12:30:44 +0200

    add subtraction operation

--- Calculator.py
+++ Calculator.py
@@ -2,6 +2,8 @@ import unittest
 class Calculator:
     def add(self, a, b):
         return a + b
+    def subtract(self, a, b):
+        return a - b
     
 class TestCalculator(unittest.TestCase):
     def setUp(self):
@@ -11,9 +13,13 @@ class TestCalculator(unittest.TestCase):
         a, b = 10, 10
         result = self.calculator.add(a, b)
         self.assertEqual(result, 20, "Сума 10 і 10 буде 20")
-        
+    
+    def test_add_negative_numbers(self):
+        a, b = -30, -40
+        result = self.calculator.subtract(a, b)
+        self.assertEqual(result, -70, "Віднімання -30 і -40 має бути -70")
     unittest.main()
     
 calculator = Calculator()
-result = calculator.add(10,10)
-print('Додавання:', result)
+result = calculator.subtract(-30,-40)
+print('Віднімання:', result)
